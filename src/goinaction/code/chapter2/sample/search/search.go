847645d13a68083d4a77d56f2b007749efe58cae
shanjiazhou <shanjiazhou@tencent.com>
2021-01-29 19:56:39 +0800

    fix: GO实战学习

--- src/goinaction/code/chapter2/sample/search/search.go
+++ src/goinaction/code/chapter2/sample/search/search.go
@@ -2,6 +2,7 @@ package search
 
 import (
 	"log"
+	"sync"
 )
 
 // A map of registerd matchers for searching.
@@ -10,12 +11,47 @@ var matchers = make(map[string]Matcher)
 // Run performs the search logid.
 func Run(searchTerm string) {
 	// Retrieve the list of feeds to search through.
-	_, err := RetrieveFeeds()
+	feeds, err := RetrieveFeeds()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Create an unbuffered channel to receive match results to display.
+	results := make(chan *Result)
+
+	// Setup a wait group so we can process all the feeds
+	var waitGroup sync.WaitGroup
+
+	// Set the number of goroutines we need to wait for while
+	// they process the individual feeds.
+	waitGroup.Add(len(feeds))
+
+	for _, feed := range feeds {
+		// Retrieve a matcher for the search.
+		matcher, exists := matchers[feed.Type]
+		if !exists {
+			matcher = matchers["default"]
+		}
+
+		// Launch the goroutine to perform the search.
+		go func(matcher Matcher, feed *Feed) {
+			Match(matcher, feed, searchTerm, results)
+		}(matcher, feed)
+	}
+
+	// Launch a goroutine to monitor when all the work is done.
+	go func() {
+		// Wait for everything to be processed.
+		waitGroup.Wait()
+
+		// Close the channel to signal to the Display
+		// function that we can exit the program.
+		close(results)
+	}()
+
+	// Start displaying results as they are available and
+	// return after the final result is displayed.
+	Display(results)
 
 }
 
